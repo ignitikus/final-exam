82bc51ae8389aaba68f6efacecde96b9ff01611e
NikolayK <nikolay.kim@codeimmersive.com>
2019-12-12 12:12:27 -0500

    adds solutions

--- main.js
+++ main.js
@@ -1,46 +1,123 @@
-const makeParagraph = () => {
-
+const makeParagraph = (sent) => {
+  let p = ''
+  sent.forEach(sen => p+=sen + ' ')
+  return p.trim()
 }
 
-const averageDogAge = () => {
-
+const averageDogAge = (pets) => {
+  let average = 0
+  let count = 0
+  pets.forEach(pet => {
+    if(pet.species === 'dog'){
+      average+= pet.age
+      count++
+    }
+  })
+  return average/count
 }
 
-const addToDigits = () => {
-
+const addToDigits = (num, modifier) => {
+  return Number(num.toString().split('').map(digit => {
+    return digit = (Number(digit) + modifier).toString().slice(-1)
+    }).join(''))
 }
 
-const nightOwls = () => {
-
+const nightOwls = (peeps) => {
+  return peeps.filter(person => {
+    if(person.localTime >= 100 && person.localTime <= 400 && person.asleep === false){
+    return person
+    }
+  })
+  
 }
 
-const nap = () => {
-
+const nap = (peeps) => {
+  return peeps.map(person => {
+    if(person.localTime >= 100 && person.localTime <= 400 ){
+      person.asleep = true
+      return person
+    }else {
+      return person
+    }
+  })
 }
 
-const findIndices = () => {
-
+const findIndices = (nums, func) => {
+  const arr =[];
+  for(let i=0; i<nums.length; i++){
+    if(func(nums[i])){
+      arr.push(i)
+    }
+  }
+    return arr
 }
 
 const Faqtory = () => {
+  return {
+    questions: [],
+    length: 0,
 
+    addQuestion: function(question){
+      this.questions.push({
+        text: question,
+        answer: '',
+        id: this.length
+        })
+      this.length++
+    },
+
+    answerQuestion: function(index,answer){
+      if(this.questions[index].answer === ''){
+        this.questions[index].answer = answer
+      }
+    }
+  }
 }
 
 
 class Dog {
+  constructor(name, breed, color){
+    this.name = name;
+    this.breed = breed;
+    this.color = color;
+  }
+
+  makeNoise(){
+    return 'Woof.'
+  }
+}
+
+class GreatDane extends Dog{
+  constructor(name, color){
+    super(name, 'Great Dane', color)
+  }
+
+  makeNoise(){
+    return super.makeNoise().toUpperCase()
+  }
 
 }
 
-class GreatDane {
+class Beagle extends Dog{
+  constructor(name){
+    super(name, 'Beagle', 'white/black/brown')
+  }
+
+  makeNoise(){
+    return 'Awooooooooo.'
+  }
 
 }
 
-class Beagle {
+class Pointer extends Dog{  
+  constructor(name, color, points){
+    super(name, 'Pointer', color)
+    this.points = points
+  }
 
-}
-
-class Pointer {
-
+  point(){
+    return this.points ? 'Over there!': super.makeNoise()
+  }
 }
 
 
